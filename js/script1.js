d24f64c535e0c24a71151711749b39ad365fcfbe
Maggie Zhang <maggie.zhang.us@gmail.com>
2017-03-13 23:51:15 -0400

    responsive nav and improved profile

--- js/script1.js
+++ js/script1.js
@@ -1,26 +1,38 @@
 window.onload = function () {
-   $("#portfolio-page").show();
+   $(".pages").show();
 };
 
 $(document).ready(function(){
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
 
+   //  $("#about-btn").click(function(event){
+   //   //Your actions here
+   //   $(".pages").hide();
+   //   $(".portfolio-labels").hide();
+   //   $(".see-more").hide();
+   //   $("#about-page").show();
+   //   $("li").removeClass("active");
+   //   $("#about-btn").addClass("active");
+   // });
 
-    $("#about-btn").click(function(event){
-     //Your actions here
-     $(".pages").hide();
-     $(".portfolio-labels").hide();
-     $(".see-more").hide();
-     $("#about-page").show();
-     $("li").removeClass("active");
-     $("#about-btn").addClass("active");
-   });
+   // $("#portfolio-btn").click(function(event){
+   //  //Your actions here
+   //  $(".pages").hide();
+   //  $("#portfolio-page").show();
+   //  $("li").removeClass("active");
+   //  $("#portfolio-btn").addClass("active");
+   // });
 
-   $("#portfolio-btn").click(function(event){
-    //Your actions here
-    $(".pages").hide();
-    $("#portfolio-page").show();
-    $("li").removeClass("active");
-    $("#portfolio-btn").addClass("active");
-   });
+    $(document).scroll(function(){
+
+      scroll_start = $(this).scrollTop();
+      if(scroll_start < 300) {
+        $('#masterhead').css('background-color', '#fefefe');
+      } else {
+        $('#masterhead').css('background-color', '#fff');
+      }
+    });
 
  });
