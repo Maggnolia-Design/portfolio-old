681d24c437b3e46794af67f67506881809f93a8f
Maggie Zhang <maggie.zhang.us@gmail.com>
2017-03-14 21:17:27 -0400

    homepage

--- js/script1.js
+++ js/script1.js
@@ -1,5 +1,6 @@
 window.onload = function () {
    $(".pages").show();
+   $("li").removeClass("active");
 };
 
 $(document).ready(function(){
@@ -8,30 +9,28 @@ $(document).ready(function(){
     var offset = startchange.offset();
 
     $("#about-btn").click(function(event){
-     //Your actions here
-     $(".pages").hide();
-     $(".portfolio-labels").hide();
-     $(".see-more").hide();
-     $("#about-page").show();
-     $("li").removeClass("active");
-     $("#about-btn").addClass("active");
+      $("html, body").animate({ scrollTop: "581px" });
    });
 
    $("#portfolio-btn").click(function(event){
-    //Your actions here
-    $(".pages").hide();
-    $("#portfolio-page").show();
-    $("li").removeClass("active");
-    $("#portfolio-btn").addClass("active");
+    $("html, body").animate({ scroll_start: "1460px" });
+
    });
 
     $(document).scroll(function(){
 
       scroll_start = $(this).scrollTop();
-      if(scroll_start < 300) {
-        $('#masterhead').css('background-color', '#fefefe');
+      if(scroll_start < 590) {
+        // $('#masterhead').css('background-color', '#fff');
+        $("li").removeClass("active");
+      } else if(scroll_start <1460){
+        // $('#masterhead').css('background-color', '#eef');
+        $("li").removeClass("active");
+        $("#about-btn").addClass("active");
       } else {
-        $('#masterhead').css('background-color', '#fff');
+        // $('#masterhead').css('background-color', '#def');
+        $("li").removeClass("active");
+        $("#portfolio-btn").addClass("active");
       }
     });
 
